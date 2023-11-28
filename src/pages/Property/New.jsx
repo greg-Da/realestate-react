18ad72f7779dba3b22f6f221396cc8d904136ca4
greg-Da <gregoiredalmasso@gmail.com>
2023-11-28 17:07:15 +0100

    add switch

--- src/pages/Property/New.jsx
+++ src/pages/Property/New.jsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
 import Switch from "../../components/atoms/Switch/Switch";
+import { useEffect } from "react";
 
 export default function New() {
+    const [renting, setRenting] = useState(false)
+    useEffect(() => {
+        console.log(renting)
+        if(renting) {
+            console.log("Renting")
+        } else {
+            console.log("Selling")
+        }
+        return () => {} // cleanup function
+    
+    })
+
+    function handleChange(){
+        setRenting(!renting)
+    }
   return (
     <div className="p-6">
-      <h1>Add your property</h1>
+      <h1 className="font-bold text-3xl">Add your property</h1>
 
-      <div></div>
+      <div className="mt-5"></div>
 
-      <div><Switch/></div>
+      <div><Switch value={renting}  handleChange={handleChange}/></div>
     </div>
   );
 }
