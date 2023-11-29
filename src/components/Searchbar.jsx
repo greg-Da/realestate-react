c9da66d557e6224f90927f58be8e94f5783ffdad
greg-Da <gregoiredalmasso@gmail.com>
2023-11-29 15:50:04 +0100

    create page Search

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,8 +1,22 @@
-export default function Searchbar() {
+import { useState } from 'react'
+export default function Searchbar({value = ''}) {
+
+  const [search, setSearch] = useState(value)
+
+  function handleKey(e){
+    if (e.key === 'Enter'){
+      console.log(search)
+    }
+  }
+
+
   return (
     <div className="border-2 py-1 px-2 rounded-full shadow-lg">
       <input
         type="text"
+        value={search}
+        onKeyDown={(e) => handleKey(e)}
+        onChange={(e) => setSearch(e.target.value)}
         placeholder="Search City"
         className="border-0 outline-none bg-transparent"
       />
